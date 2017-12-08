e4eba6bf22a73b87fa0b30e4f881d8e2219cf2bb
davidbrown2324 <david.brown3@ucsf.edu>
2017-12-08 15:16:04 -0800

    Update Proline.R

--- Proline.R
+++ Proline.R
@@ -4,11 +4,7 @@ library("Biostrings", lib.loc="~/R/win-library/3.2")
 s<-as.character(unique(foci_pep$uniprot_sequence))
 s<-AAStringSet(s)
 
-
-#Proline content
-
-#Proline spacing
-
+#Define the proline spacing function, which returns 'IDR_width', 'Proline_Count', 'Proline_Fraction', 'Mean_Spacing' and 'Spacing_StdDev'
 proline_spacing_fun<-function(s){
   IDR_composition<-alphabetFrequency(s)
   P_cont<-IDR_composition[,"P"]
@@ -18,18 +14,11 @@ proline_spacing_fun<-function(s){
   list(width(s), P_cont, P_frac, mean(P_space), sd(P_space))
 }
 
+#Call 'proline_spacing_fun' on a sequence 's'
 pro<-data.frame(proline_spacing_fun(s))
 names(pro)<-c("IDR_width", "Proline_Count", "Proline_Fraction", "Mean_Spacing", "Spacing_StdDev")
 
+#Subset proteins between 30 and 100 aa with >30% prolines
 clean<-pro[pro$IDR_width>30 & pro$IDR_width<100 & pro$Proline_Fraction>0.3,]
 plot(clean$Proline_Fraction, clean$Mean_Spacing)
 plot(clean$Proline_Count, clean$Spacing_StdDev)
-
-head(search, 2)
-head(start(search), 2)
-head(diff(start(search)), 2)
-head(mode(diff(start(search))), 2)
-
-head(s, 10)
-
-mean(P_space)
